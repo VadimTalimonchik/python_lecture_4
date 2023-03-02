4cdc2f0812f195a4f8e3d9a7a5f0047ebdea363d
VadimTalimonchik <tvadmins@gmail.com>
2023-03-02 09:20:49 +0300

    калькулятор

---
+++ v.2/lec.py
@@ -0,0 +1,32 @@
+# def f(x):
+#     return x ** 2
+# print(f(2))
+# print(type(f))
+
+# a = f
+# print(type(a))
+# print(a(5))
+
+# ------------------
+# def calk1(a):
+#     return a + a
+
+# def calk2(a):
+#     return a * a
+
+# def math(op, x):
+#     print(op(x))
+
+# math(calk2, 5)
+
+# ------------------
+# def calk1(a, b):
+#     return a + b
+
+# def calk2(a, b):
+#     return a * b
+
+# def math(op, x, y):
+#     print(op(x, y))
+
+# math(calk1, 5, 45)
