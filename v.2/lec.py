0830c10205695a4998c459cf4f8ec12397da1f16
VadimTalimonchik <tvadmins@gmail.com>
2023-03-04 22:01:25 +0300

    ФУНКЦИЯ filter

--- v.2/lec.py
+++ v.2/lec.py
@@ -102,17 +102,41 @@
 # data = list(map(int, data.split()))
 # print(data)
 
-# отсылка к прошлой задаче
-# def select(f, col):
-#     return[f(x) for x in col]
+# отсылка к прошлой задаче: 
+# функцию select, которая прописана явно,
+# заменим на встроенную функцию map
 
-def where(f, col):
-    return[x for x in col if f(x)]
+# # def select(f, col):
+# #     return[f(x) for x in col]
+
+# def where(f, col):
+#     return[x for x in col if f(x)]
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = map(int, data)
+# print(res)
+# res = where(lambda x: x % 2 == 0, res)
+# print(res)
+# res = list(map(lambda x: (x, x ** 2), res))
+# print(res)
+
+# ФУНКЦИЯ filter
+data = [15, 65, 9, 36, 175] # создаём список с различными элементами в виде чисел
+res = list(filter(lambda x: x % 10 == 5, data)) # применяем функцию filter и делаем выборку
+                                                # чисел которые заканчиваются на 5.
+print(res)
+
+# обратно отсылка к прошлой задаче: 
+# функцию where, которая прописана явно,
+# заменим на встроенную функцию filter
+
+# def where(f, col):
+#     return[x for x in col if f(x)]
 
 data = [1, 2, 3, 5, 8, 15, 23, 38]
 res = map(int, data)
 print(res)
-res = where(lambda x: x % 2 == 0, res)
+res = filter(lambda x: x % 2 == 0, res)
 print(res)
 res = list(map(lambda x: (x, x ** 2), res))
 print(res)
