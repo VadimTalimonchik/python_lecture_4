13f69b492279924d65b20eefc04fe24aa52b04ed
VadimTalimonchik <tvadmins@gmail.com>
2023-03-04 22:54:04 +0300

    ФУНКЦИЯ enumerate

--- v.2/lec.py
+++ v.2/lec.py
@@ -147,8 +147,14 @@
 # print(data) # => [('user1', 4), ('user2', 5), ('user3', 9), ('user4', 14), ('user5', 7)]
 
 # Функция zip () пробегает по минимальному входящему набору:
-users = ['user1', 'user2', 'user3', 'user4', 'user5']
-ids = [4, 5, 9, 14, 7]
-salary = [111, 222, 333]
-data = list(zip(users, ids, salary))
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]
+# data = list(zip(users, ids, salary))
+# print(data)
+
+# ФУНКЦИЯ enumerate
+# Функция enumerate() позволяет пронумеровать набор данных.
+users = ['user1', 'user2', 'user3']
+data = list(enumerate(users))
 print(data)
